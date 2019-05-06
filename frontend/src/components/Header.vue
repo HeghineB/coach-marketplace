01f02857be7f7289471b35e45842897ad61e7802
Joel <camacho-joel@hotmail.com>
2019-05-06 22:46:24 +0200

    styling elements

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="hello">
-    <img src="../assets/images/runner.png" class="header-image">
-    <nav>
-      <a href="#">Home</a>
-      <a href="#">About</a>
-      <a href="#">Contact</a>
-      <a href="#">Login</a>
+  <div class="header-container">
+    <div class="header-logo">
+      <img src="../assets/images/runner.png" class="header-image">
+      <h2 class="header-title">Coach Marketplace</h2>
+    </div>
+    <nav class="header-navbar">
+      <a href="#" class="header-button">Home</a>
+      <a href="#" class="header-button">About</a>
+      <a href="#" class="header-button">Contact</a>
+      <a href="#" class="header-button">Login</a>
     </nav>
   </div>
 </template>
@@ -16,7 +19,38 @@ export default {};
 
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700");
+.header-container {
+  font-family: "Quicksand";
+  max-width: 100%;
+  max-height: 70px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  background-color: #009484;
+}
+.header-logo {
+  display: flex;
+  flex-direction: row;
+}
 .header-image {
+  padding-top: 10px;
+  width: 50px;
+  height: 50px;
+}
+.header-title {
+  color: white;
+  height: 40px;
+  width: 300px;
+}
+.header-navbar {
+  padding-top: 25px;
+}
+.header-button {
+  color: white;
+  padding-top: 25px;
+  padding-right: 10%;
+  margin: 5%;
 }
 </style>
 
