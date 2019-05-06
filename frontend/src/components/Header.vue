ee3e24dd40362df61cfabc9e044655e29a6eacb1
Joel <camacho-joel@hotmail.com>
2019-05-06 22:02:36 +0200

    adding navbar buttons

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="hello">
     <img src="../assets/images/runner.png" class="header-image">
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a>
+      <a href="#">Login</a>
+    </nav>
   </div>
 </template>
 
