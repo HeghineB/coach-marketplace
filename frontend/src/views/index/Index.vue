e38fd7152dde6995e849bf72ba4bd5a02b6dc47c
Kevin Tassi <kevin@fishtripr.com>
2019-05-18 18:03:12 +0200

    add(core): vue-router

---
+++ frontend/src/views/index/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="index-page">
+    Here insert your / content
+  </div>
+</template>
+
+<script>
+// import Foo from 'bar/foo'
+
+export default {
+  name: 'index-page',
+  mixins: [],
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {},
+  watch: {},
+  beforeCreated() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {}
+}
+</script>
+
+<style scoped>
+.index-page {}
+</style>
