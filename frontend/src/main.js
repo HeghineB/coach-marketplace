ca790b16de52fca7838b315feaf2ae8848bd291a
Joel <camacho-joel@hotmail.com>
2019-05-18 12:39:19 +0200

    installing material design icons package

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,19 @@ import Vue from "vue";
 import App from "./App.vue";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.use(Vuetify);
 Vue.config.productionTip = false;
+
+Vue.use(Vuetify, {
+  iconfont: "md"
+});
+
+Vue.use(Vuetify, {
+  iconfont: "mdi"
+});
 
 new Vue({
   render: h => h(App)
