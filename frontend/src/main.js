501e5ef305cc54b3e053ff2258002abfd4504463
Joel <camacho-joel@hotmail.com>
2019-05-18 11:54:02 +0200

    adding vue router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
+import VueRouter from "vue-router";
 
+Vue.use(VueRouter);
 Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
