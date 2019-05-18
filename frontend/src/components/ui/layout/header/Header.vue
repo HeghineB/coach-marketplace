b654bc1acef40dc66f3df757a9bee586e800c10e
Kevin Tassi <kevin@fishtripr.com>
2019-05-18 18:15:07 +0200

    add header structure component

---
+++ frontend/src/components/ui/layout/header/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-header">
+
+  </div>
+</template>
+
+<script>
+// import Foo from 'bar/foo'
+
+export default {
+  name: 'main-header',
+  mixins: [],
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {},
+  watch: {},
+  beforeCreated() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {}
+}
+</script>
+
+<style scoped>
+.main-header {}
+</style>
