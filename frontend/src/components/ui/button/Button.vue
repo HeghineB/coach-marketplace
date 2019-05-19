5b30dab84863d4bbbc62506748885ca060162529
IgorZ07 <wow0707@gmail.com>
2019-05-19 11:39:59 +0200

    button component added WIP

---
+++ frontend/src/components/ui/button/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="name"></div>
+</template>
+
+<script>
+// import Foo from 'bar/foo'
+
+export default {
+  name: "button",
+  mixins: [],
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  watch: {},
+  beforeCreated() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {}
+};
+</script>
+
+<style scoped>
+.name {
+}
+</style>
