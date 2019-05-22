5969d4b0df8f827867d32e4df5a095fc819e298f
Joel <camacho-joel@hotmail.com>
2019-05-22 23:26:39 +0200

    installing packages for rendering icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/index'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router/index";
+import Vuetify from "vuetify";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "@mdi/font/css/materialdesignicons.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 // Use Vuetify
-import './plugins/vuetify'
+import "./plugins/vuetify";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuetify, {
+  iconfont: "mdi"
+});
+
+Vue.use(Vuetify, {
+  iconfont: "md"
+});
+
+Vue.use(Vuetify, {
+  iconfont: "fa"
+});
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
