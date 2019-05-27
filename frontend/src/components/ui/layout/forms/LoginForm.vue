d4fa39eee54a8f3f55becd38f454a2476405576d
Joel <camacho-joel@hotmail.com>
2019-05-27 23:05:00 +0200

    creating log in component and adding vee-validate package for validation

---
+++ frontend/src/components/ui/layout/forms/LoginForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="name"></div>
+</template>
+
+<script>
+// import Foo from 'bar/foo'
+
+export default {
+  name: "name",
+  mixins: [],
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  watch: {},
+  beforeCreated() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {}
+};
+</script>
+
+<style  scoped>
+@import "";
+
+.name {
+}
+</style>
