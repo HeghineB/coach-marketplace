0743dc32653c2bc2496cee8a1f0b860110041002
IgorZ07 <wow0707@gmail.com>
2019-07-26 14:28:27 +0200

    Separate component for render each country added

---
+++ frontend/src/components/ui/CountryItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <li v-model="country">
+    <input @keyup.enter="changeCountryName" :value="country.name" />
+    <button class="delButton" @click="removeCountry(country)">x</button>
+  </li>
+</template>
+
+<script>
+// import Foo from 'bar/foo'
+
+export default {
+  name: "CountryItem",
+  mixins: [],
+  components: {},
+  props: ["country"],
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {
+    removeCountry(element) {
+      console.log(element);
+      this.$store.dispatch("deleteCountry", element.id);
+    },
+    changeCountryName(element) {
+      let changedCountry = { id: this.country.id, name: element.target.value };
+      this.$store.dispatch("changeCountry", changedCountry);
+    }
+  },
+  watch: {},
+  beforeCreated() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {}
+};
+</script>
+
+<style scoped>
+</style>`
