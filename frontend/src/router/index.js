25bbf9f350e216738b2c4ed4f8df296c82a8b652
Joel <camacho-joel@hotmail.com>
2019-07-27 23:33:57 +0200

    defining routes for each component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,3 +1,16 @@
-const routes = [];
+
+import HomePage from "./components/HomePage";
+import SearchCoachPage from "./components/SearchCoachPage";
+import ContactPage from "./components/ContactPage";
+import ShopPage from "./components/ShopPage";
+
+const routes = [
+  { path: "/", component: HomePage},
+  { path: "/searchCoach", component: SearchCoachPage},
+  { path: "/contactPage", component: ContactPage},
+  { path: "/shopPage", component: ShopPage}
+];
+];
+];
 
 export default routes;
