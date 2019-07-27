aee7a9fd4c11637a7a5a9359fefc4801e8ced76c
Joel <camacho-joel@hotmail.com>
2019-07-27 23:20:09 +0200

    installing vue-router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
