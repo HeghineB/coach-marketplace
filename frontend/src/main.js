f5480de3e0222e561129b9e03c3304e38cb0ef29
Joel <camacho-joel@hotmail.com>
2019-07-27 23:25:04 +0200

    setting up router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
 
+import routes from "./router/index";
+
 Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  routes
 }).$mount("#app");
